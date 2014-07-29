9213a8b1b8b5886e9db188ef27ed659aaaaa1c27
Brian Grivna <sorghumking@yahoo.com>
2014-07-29 12:40:52 -0500

    Version to 1.8.0; funcs to make short and long version strings

--- app/version.py
+++ app/version.py
@@ -1,5 +1,23 @@
-BaseVersion = "1.7"
-VersionSuffix = "a10"
-LongVersionSuffix = "alpha 10"
-ShortVersion = BaseVersion + "_" + VersionSuffix
-LongVersion = BaseVersion + " " + LongVersionSuffix
+BaseVersion = "1.8.0"
+VersionSuffix = ""
+LongVersionSuffix = ""
+
+def GetShortVersion():
+	if VersionSuffix != "":
+		return BaseVersion + "_" + VersionSuffix
+	else:
+		return BaseVersion
+
+def GetLongVersion():
+	if LongVersionSuffix != "":
+		return BaseVersion + " " + LongVersionSuffix
+	else:
+		return BaseVersion
+
+ShortVersion = GetShortVersion()
+LongVersion = GetLongVersion()
+
+
+if __name__ == "__main__":
+	print "GetShortVersion() = " + GetShortVersion()
+	print "GetLongVersion() = " + GetLongVersion()
