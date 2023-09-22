3aa10d91b32a4d5f915d3fccf517457edefcfeb9
Brian G <sorghumking@yahoo.com>
2023-09-22 18:33:25 -0500

    Build dylib on Monterey

--- app/yosemiteBuild.sh
+++ app/yosemiteBuild.sh
@@ -1,6 +1,10 @@
-# script to get a working build of C++ code on Yosemite
+# script to get a working build of C++ code on Yosemite...and now Monterey (August 23 2023).
+# Requires the 'core' library (C++ files in correlator/src built with `make install`).
+#
+# Had to update include path to point to Homebrew Python 3.11. Command `find /usr/local/Cellar -name Python.h`
+# is quite useful!
 
-g++ -fno-strict-aliasing -fno-common -dynamic -I../include -DCORRELATOR_MACOSX -I /Library/Frameworks/Python.framework/Versions/3.9/include/python3.9/ -c -ggdb py_func.cpp
+g++ -fno-strict-aliasing -fno-common -dynamic -I../include -DCORRELATOR_MACOSX -I /usr/local/Cellar/python@3.11/3.11.3/Frameworks/Python.framework/Versions/3.11/include/python3.11/ -c -ggdb py_func.cpp
 
 g++ -bundle -undefined dynamic_lookup py_func.o -L /Users/bgrivna/proj/corewall/correlator/lib -lcore -o py_correlator.so
 
